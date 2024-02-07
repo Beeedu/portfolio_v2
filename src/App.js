e355ea70ec144020a4521aeb8610a97403b81a2c
blynch <bralyn99@gmail.com>
2024-02-07 09:28:53 -0800

    reroute to about by default

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { PortfolioPage } from './portfolio/PortfolioPage';
 import theme from './theme';
 import { ThemeProvider } from '@mui/material/styles';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { AboutOutlet } from './portfolio/AboutOutlet';
 import { ContactOutlet } from './portfolio/ContactOutlet';
 import { ProjectsOutlet } from './portfolio/projects/ProjectsOutlet';
@@ -13,7 +13,10 @@ function App() {
             <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Routes>
                     <Route path="/" element={<PortfolioPage />}>
-                        <Route index element={<AboutOutlet />} />
+                        <Route
+                            index
+                            element={<Navigate to="about" replace />}
+                        />
                         <Route path="about" element={<AboutOutlet />} />
                         <Route path="projects" element={<ProjectsOutlet />} />
                         <Route path="contact" element={<ContactOutlet />} />
